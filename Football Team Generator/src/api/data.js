725f0392895ae733a2fbedba533b5749df129e3d
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-04-18 14:28:25 +0300

    Add files via upload

--- Football Team Generator/src/api/data.js
+++ Football Team Generator/src/api/data.js
@@ -40,6 +40,10 @@ async function searchTeamByName(teamName){
     return api.get(`/data/teams?where=name LIKE ${teamName}`)
 }
 
+async function getSpecificLike(teamId, userId){
+    return api.get(`/data/likes?where=teamId%3D%22${teamId}%22%20and%20_ownerId%3D%22${userId}%22&count`)
+}
+
 export {
     login,
     register,
@@ -52,5 +56,6 @@ export {
     getMyTeams,
     addLike,
     getAllLikes,
-    searchTeamByName
+    searchTeamByName,
+    getSpecificLike
 }
