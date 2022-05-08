8c92cd3cef0dcb9a486fd55cbbb7e21b466b8fea
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-05-08 15:15:56 +0300

    Add files via upload

--- Battleship Game/Battleship/Coordinates.cs
+++ Battleship Game/Battleship/Coordinates.cs
@@ -8,6 +8,13 @@ namespace Battleship
 {
     public class Coordinates
     {
+        public Coordinates(int row, int column)
+        {
+            this.Row = row;
+            this.Column = column;
+        }
 
+        public int Row { get; set; }
+        public int Column { get; set; } 
     }
 }
--- Battleship Game/Battleship/OccupationType.cs
+++ Battleship Game/Battleship/OccupationType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,29 @@ namespace Battleship
 {
     public enum OccupationType
     {
+        [Description("o")]
+        Empty,
+
+        [Description("B")]
+        Battleship,
+
+        [Description("C")]
+        Cruiser,
+
+        [Description("D")]
+        Destroyer,
+
+        [Description("S")]
+        Submarine,
+
+        [Description("A")]
+        Carrier,
+
+        [Description("X")]
+        Hit,
+
+        [Description("M")]
+        Miss
 
     }
 }
