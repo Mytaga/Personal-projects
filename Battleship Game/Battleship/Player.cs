c40e46d8bd2289cbd7f7793cbdfd3717c6751ec3
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-05-09 23:08:33 +0300

    Add files via upload

--- Battleship Game/Battleship/Player.cs
+++ Battleship Game/Battleship/Player.cs
@@ -8,6 +8,25 @@ namespace Battleship
 {
     public class Player
     {
+        public Player(string name)
+        {
+            this.Name = name;
+            this.Board = new GameBoard();
+            this.FiringBoard = new FiringBoard();
+            this.Ships = new List<Ship>()
+            {
+                new Destroyer(),
+                new Submarine(),
+                new Cruiser(),
+                new Battleship(),
+                new Carrier()
+            };
+        }
 
+        public string Name { get; set; }
+        public GameBoard Board { get; set; }
+        public FiringBoard FiringBoard { get; set; }    
+        public List<Ship> Ships { get; set; }
+        public bool HasLost => this.Ships.All(x => x.IsSunk);
     }
 }
