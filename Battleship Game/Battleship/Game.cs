5516ed96270d0f907f5fb619b3b8276624da2d7e
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-05-10 21:21:23 +0300

    Add files via upload

--- Battleship Game/Battleship/Game.cs
+++ Battleship Game/Battleship/Game.cs
@@ -8,12 +8,14 @@ namespace Battleship
 {
     public class Game
     {
-        public Player Player1 { get; set; } 
-        public Player Player2 { get; set; } 
+        
         public Game()
         {
 
         }
+
+        public Player Player1 { get; set; }
+        public Player Player2 { get; set; }
 
         public void PlayRound()
         {
