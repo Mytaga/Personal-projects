8d35799a871962cb48f73848c373401614ea9061
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-05-11 22:16:05 +0300

    Add files via upload

---
+++ Battleship Game/Battleship/PanelExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    public static class PanelExtensions
+    {
+        public static List<Panel> Range(this List<Panel> panels, int startRow, int startColumn, int endRow, int endColumn)
+        {
+            return panels.Where(x => x.Coordinates.Row >= startRow 
+            && x.Coordinates.Column >= startColumn && x.Coordinates.Row <= endRow 
+            && x.Coordinates.Column <= endColumn).ToList();
+        }
+        public static Panel At(this List<Panel> panels, int row, int column)
+        {
+            return panels.Where(x => x.Coordinates.Row == row && x.Coordinates.Column == column).First();
+        }
+    }
+}
--- Battleship Game/Battleship/Player.cs
+++ Battleship Game/Battleship/Player.cs
@@ -28,5 +28,81 @@ namespace Battleship
         public FiringBoard FiringBoard { get; set; }    
         public List<Ship> Ships { get; set; }
         public bool HasLost => this.Ships.All(x => x.IsSunk);
+
+        public void PlaceShips()
+        {
+            Random rand = new Random(Guid.NewGuid().GetHashCode());
+
+            foreach (var ship in this.Ships)
+            {
+                bool isOpen = true;
+
+                while (isOpen)
+                {
+                    var startColumn = rand.Next(1, 11);
+                    var startRow = rand.Next(1, 11);
+                    int endRow = startRow;
+                    int endColumn = startColumn;
+                    var orientation = rand.Next(1, 101) % 2;
+
+                    List<int> panelNumbers = new List<int>();
+                    if (orientation == 0)
+                    {
+                        for (int i = 0; i < ship.Width; i++)
+                        {
+                            endRow++;
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < ship.Width; i++)
+                        {
+                            endColumn++;
+                        }
+                    }
+
+                    if (endRow > 10 || endColumn > 10)
+                    {
+                        isOpen = true;
+                        continue; 
+                    }
+                    
+                    var affectedPanels = this.Board.Panels.Range(startRow, startColumn, endRow, endColumn);
+
+                    if (affectedPanels.Any(p => p.IsOccupied))
+                    {
+                        isOpen = true;
+                        continue;
+                    }
+
+                    foreach (var panel in affectedPanels)
+                    {
+                        panel.OccupationType = ship.OccupationType;
+                    }
+
+                    isOpen = false;
+                }
+            }
+        }
+
+        public void OutputBoards()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine("Own Board:                          Firing Board:");
+            for (int row = 1; row <= 10; row++)
+            {
+                for (int ownColumn = 1; ownColumn <= 10; ownColumn++)
+                {
+                    Console.Write(Board.Panels.At(row, ownColumn).Status + " ");
+                }
+                Console.Write("                ");
+                for (int firingColumn = 1; firingColumn <= 10; firingColumn++)
+                {
+                    Console.Write(FiringBoard.Panels.At(row, firingColumn).Status + " ");
+                }
+                Console.WriteLine(Environment.NewLine);
+            }
+            Console.WriteLine(Environment.NewLine);
+        }
     }
 }
