c7ecaec63a66aa940854183d02521f25b437fcf1
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-05-14 15:36:22 +0300

    Add files via upload

--- Battleship Game/Battleship/Program.cs
+++ Battleship Game/Battleship/Program.cs
@@ -10,7 +10,29 @@ namespace Battleship
     {
         static void Main(string[] args)
         {
+            int player1Wins = 0;
+            int player2Wins = 0;
 
+            Console.WriteLine("How many games do you want to play?");
+            var numGames = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numGames; i++)
+            {
+                Game game1 = new Game();
+                game1.PlayToEnd();
+                if (game1.Player1.HasLost)
+                {
+                    player2Wins++;
+                }
+                else
+                {
+                    player1Wins++;
+                }
+            }
+
+            Console.WriteLine("Player 1 Wins: " + player1Wins.ToString());
+            Console.WriteLine("Player 2 Wins: " + player2Wins.ToString());
+            Console.ReadLine();
         }
     }
 }
