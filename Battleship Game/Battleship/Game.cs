4cba2f7589de0d1273c110b5d5dddf008cdf8a93
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-05-14 14:50:49 +0300

    Add files via upload

--- Battleship Game/Battleship/Game.cs
+++ Battleship Game/Battleship/Game.cs
@@ -11,7 +11,14 @@ namespace Battleship
         
         public Game()
         {
+            this.Player1 = new Player("Mitko");
+            this.Player2 = new Player("Boryana");
 
+            this.Player1.PlaceShips();
+            this.Player2.PlaceShips();
+
+            this.Player1.OutputBoards();
+            this.Player2.OutputBoards();
         }
 
         public Player Player1 { get; set; }
@@ -19,11 +26,32 @@ namespace Battleship
 
         public void PlayRound()
         {
+            var coordinates = Player1.FireShot();
+            var result = Player2.ProcessShot(coordinates);
+            Player1.ProcessShotResult(coordinates, result);
 
+            if (!Player2.HasLost)
+            {
+                coordinates = Player2.FireShot();
+                result = Player1.ProcessShot(coordinates);
+                Player2.ProcessShotResult(coordinates, result);
+            }
         }
         public void PlayToEnd()
         {
+            while (!Player1.HasLost && !Player2.HasLost)
+            {
+                PlayRound();
+            }
 
+            if (Player1.HasLost)
+            {
+                Console.WriteLine(Player2.Name + " has won the game!");
+            }
+            else if (Player2.HasLost)
+            {
+                Console.WriteLine(Player1.Name + " has won the game!");
+            }
         }
     }
 }
